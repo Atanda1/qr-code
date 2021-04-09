6189cf50ee5dea4ca6f4292f365f3f8a1c52b407
David Atanda <atandadave@gmail.com>
2021-04-09 14:09:39 +0100

    QR Camera now displays

--- src/components/qrScanner.vue
+++ src/components/qrScanner.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="hello">
-    <a ref="qr-btn" @click="scan" >
-      <img alt="Vue logo" src="../assets/qrcode.jpg">
-     
-      <video v-if="scanning" ref="video" :src="videourl"  id="thevideo">
-      </video>
-    </a>
+    <div class="video">
+      <video autoplay></video>
+    </div>
+   
 
     <canvas v-if="scanning" ref="qr-canvas"></canvas>
-     <img class="arrow" src="../assets/arrow-up.png" />
+    <img class="arrow" src="../assets/arrow-up.png" />
     <h3>Click the bar-code to scan</h3>
     <div ref="result" v-if="qrScanResult">
       <b>Data:</b> <span id="outputData">{{ result }}</span>
@@ -18,46 +16,57 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      scanning : false,
+      scanning: false,
       qrcode: null,
       qrScanResult: false,
       result: "",
       image: true,
-      videourl : ""
-    }
+      videourl: "",
+    };
   },
   methods: {
     scan() {
-      let self = this
-      //this.scanning = true;
-      this.image= false;
-      const video = document.getElementById('#thevideo');
-      console.log(video)
-      this.qrScanResult = true;
-      navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: "environment" } })
-      .then(function(stream) {
-         self.scanning = true;
-        self.qrScanResult = true;
-        //btnScanQR.hidden = true;
-        //canvasElement.hidden = false;
-        // const video = this.$refs.video;
-        self.videourl = stream;
-        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-        
-        video.play();
-        this.tick();
-        this.theScan();
-      });
+      if (
+        "mediaDevices" in navigator &&
+        "getUserMedia" in navigator.mediaDevices
+      ) {
+        //let self = this;
+        //this.scanning = true;
+        // this.image = false;
+        // const video = document.querySelector("video");
+        // console.log(video);
+        // this.qrScanResult = true;
+        let constraints = {
+          video: {
+            width: { min: 80 },
+            height: { min: 50 },
+          },
+        };
+        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+          console.log(stream);
+          const video = document.querySelector('video');
+          //self.scanning = true;
+          //self.qrScanResult = true;
+          //btnScanQR.hidden = true;
+          //canvasElement.hidden = false;
+          // const video = this.$refs.video;
+          video.srcObject = stream;
+          video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+
+          video.play();
+          // this.tick();
+          // this.theScan();
+        });
+      }
     },
     tick() {
-      const canvasElement = this.$refs.qr-canvas;
+      const canvasElement = this.$refs.qr - canvas;
       const canvas = canvasElement.getContext("2d");
       const video = this.$refs.video;
       canvasElement.height = video.videoHeight;
@@ -72,7 +81,7 @@ export default {
       } catch (e) {
         setTimeout(this.theScan(), 300);
       }
-    }
+    },
   },
   watch: {
     qrcode: function() {
@@ -82,27 +91,28 @@ export default {
           this.result = res;
           this.scanning = false;
 
-          video.srcObject.getTracks().forEach(track => {
+          video.srcObject.getTracks().forEach((track) => {
             track.stop();
           });
 
           this.qrScanResult = false;
         }
       };
-    }
+    },
   },
+  beforeMount: function () {
+		this.scan();
+	},
   mounted() {
-    console.log(window.qrcode)
-    this.qrcode = window.qrcode
-
+    console.log(window.qrcode);
+    this.qrcode = window.qrcode;
 
     // const canvasElement = this.$refs.qr-canvas;
     // const canvas = canvasElement.getContext("2d");
     // console.log(canvasElement)
     //const outputData = this.$refs.outputData;
-
-  }
-}
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -128,5 +138,18 @@ a {
 }
 img {
   height: 25rem;
+}
+
+
+@media only screen and (max-width: 600px) {
+  .video {
+    width: 80vw;
+    margin: 0 auto;
+  }
+  video {
+    max-width: 100%;
+    max-height: 100%;
+    overflow: hidden;
+  }
 }
 </style>
